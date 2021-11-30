0aadb7eecf727cb20e0614f3770e05e7787265ea
JesperHvilsted <jesper9898@gmail.com>
2021-11-30 14:41:40 +0100

    opdateringer

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ let opretProfilVogn = []
 let draw = false
 let isProfileCreated = false
 
-const profilVogn = [
+const profilVogn1 = [
   [1000,1600],[1200,1600],
   [1200, 1200], [1320, 1200],
   [1320, 1080], [1200,1080],
@@ -69,6 +69,13 @@ const profilVogn = [
   [800, 1080], [600, 1080],
   [600, 1360], [800, 1360],
   [800, 1600], [1000,1600]
+]
+
+const profilVogn = [
+  [1000, 1600],[1300, 1600],
+  [1300, 1300], [1400, 1300],
+  [1400, 1200], [1300, 1200],
+  [1300, 1000], [1000, 1000]
 ]
 
 const sideprofil1 = [
@@ -109,6 +116,7 @@ function endDraw(){
     svgDrawProfile.removeChild(svgDrawProfile.lastChild)
     opretProfilVogn.push(opretProfilVogn[0]) //Ending the profile by adding the coordinates as the last coordinates. 
     finaleProfile = convertProfilToSvgFormat(opretProfilVogn);
+    lineOfProfil = document.createElementNS(NS, 'polyline')
     lineOfProfil.setAttribute("points" , finaleProfile);
     lineOfProfil.setAttribute("stroke", "black");
     lineOfProfil.setAttribute("fill", "none");
@@ -122,19 +130,19 @@ function removeLastPoint (){
     svgDrawProfile.removeChild(svgDrawProfile.lastChild)
     opretProfilVogn.pop()
     finaleProfile = convertProfilToSvgFormat(opretProfilVogn);
-    lineOfProfil.setAttribute("points" , finaleProfile);
-    lineOfProfil.setAttribute("stroke", "black");
-    lineOfProfil.setAttribute("fill", "none");
-    lineOfProfil.setAttribute('id', "testID")
-    svgDrawProfile.appendChild(lineOfProfil);
+    lineOfProfil1 = document.createElementNS(NS, 'polyline')
+    lineOfProfil1.setAttribute("points" , finaleProfile);
+    lineOfProfil1.setAttribute("stroke", "black");
+    lineOfProfil1.setAttribute("fill", "none");
+    lineOfProfil1.setAttribute('id', "testID")
+    svgDrawProfile.appendChild(lineOfProfil1);
   }
 }
 
-// add a circle to the target
 function drawPointInProfile(event) {
 if(draw){
 
-  // add circle to containing element
+ 
   const target = event.target.closest('g') || event.target.ownerSVGElement || event.target //Target er det element man trykker på. Her er det vores svg element. 
 
   //clientX & Y er eventets koordinater. Dette er et pointer down, så det er der, hvor der klikkes. Det i forhold til screen width and height
@@ -173,14 +181,26 @@ svgY.textContent = "Y: " + svgYTextContent
 svgX.textContent = "X: " + svgXTextContent
 }
 
-// Hvis tid kan der bygges videre på denne funktion. 
-/*
-function convertFromSvgToProfileCoordinates(){
-  var box = svg.viewBox.baseVal
-  svgYTextContent = Math.abs(svgYTextContent - box.height)
-  svgXTextContent = svgXTextContent - box.height/2
+function fullSpeed(profil){
+  newProfile = []
+  for(i = 0; i < profil.length; i++){
+    test = convertFromSvgToProfileCoordinates(profil[i][0], profil[i][1])
+    console.log(test[1])
+  }
+  //y = profil[i][1] //Using height in the formel
+  y = 4300
+  newX = ((y-850)/2000 * 25) + (y*15/1500) + (10/2) * 1.5 + 30 + 2.5 //The calculated x for extra width of profile
+  console.log("X er: " + newX)
 }
-*/
+
+// Hvis tid kan der bygges videre på denne funktion. 
+function convertFromSvgToProfileCoordinates(x,y){
+  var box = svg.viewBox.baseVal
+  newY = Math.abs(y - box.height)
+  newX = x - box.height/2
+
+  return [newX, newY]
+}
 
 
 // translate page to SVG co-ordinate
@@ -194,6 +214,7 @@ function svgPoint(element, x, y) {
 
 function chooseProfile(){
   if(this.id == "btnIndlæsknap"){
+    fullSpeed(profilVogn)
     displayProfil(profilVogn, this.id)
   }
   if(this.id == "btnUseDrawProfile"){
@@ -220,18 +241,55 @@ function displayProfil(profilVogn, id){
       newProfile = drawProfile(convertedProfil, id)
       newProfile1 = drawShadowProfile(asymmetriskProfilOriginal, "shadowProfil1")
       newProfile2 = drawShadowProfile(asymmetriskProfil, "shadowProfil2")
+
       svg.appendChild(newProfile)
       svg.appendChild(newProfile1)
       svg.appendChild(newProfile2)
+
       svg.replaceChild(newProfile,child)
       svg.replaceChild(newProfile1,svg.children[i-2])
       svg.replaceChild(newProfile2,svg.children[i-1])
+
+
+      newProfilek = drawProfile(convertedProfil, id)
+      newProfile1k = drawShadowProfile(asymmetriskProfilOriginal, "shadowProfil1")
+      newProfile2k = drawShadowProfile(asymmetriskProfil, "shadowProfil2")
+
+      svgProfileOnly.appendChild(newProfilek)
+      svgProfileOnly.appendChild(newProfile1k)
+      svgProfileOnly.appendChild(newProfile2k)
+
+      svgProfileOnly.replaceChild(newProfilek,child)
+      svgProfileOnly.replaceChild(newProfile1k,svg.children[i-2])
+      svgProfileOnly.replaceChild(newProfile2k,svg.children[i-1])
     }
     if(child.id == "btnUseDrawProfile"){
       console.log("demoprofil1")
+
       newProfile = drawProfile(convertedProfil, id)
+      newProfile1 = drawShadowProfile(asymmetriskProfilOriginal, "shadowProfil1")
+      newProfile2 = drawShadowProfile(asymmetriskProfil, "shadowProfil2")
+
       svg.appendChild(newProfile)
+      svg.appendChild(newProfile1)
+      svg.appendChild(newProfile2)
+
       svg.replaceChild(newProfile,child)
+      svg.replaceChild(newProfile1,svg.children[i-2])
+      svg.replaceChild(newProfile2,svg.children[i-1])
+
+
+      newProfilek = drawProfile(convertedProfil, id)
+      newProfile1k = drawShadowProfile(asymmetriskProfilOriginal, "shadowProfil1")
+      newProfile2k = drawShadowProfile(asymmetriskProfil, "shadowProfil2")
+
+      svgProfileOnly.appendChild(newProfilek)
+      svgProfileOnly.appendChild(newProfile1k)
+      svgProfileOnly.appendChild(newProfile2k)
+
+      svgProfileOnly.replaceChild(newProfilek,child)
+      svgProfileOnly.replaceChild(newProfile1k,svg.children[i-2])
+      svgProfileOnly.replaceChild(newProfile2k,svg.children[i-1])
     }
   }
  
@@ -246,7 +304,7 @@ function displayProfil(profilVogn, id){
 
     newProfile1 = drawShadowProfile(asymmetriskProfilOriginal, "shadowProfil1")
     newProfile2 = drawShadowProfile(asymmetriskProfil, "shadowProfil2")
-    newProfile3 = drawProfile(convertedProfil, "profil")
+    newProfile3 = drawProfile(convertedProfil, id)
 
     svgProfileOnly.appendChild(newProfile1)
     svgProfileOnly.appendChild(newProfile2)
